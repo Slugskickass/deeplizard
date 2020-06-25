44df8b7dab22eeab276281399e6c8bb30e1719dc
CadbyLab <48517894+CadbyLab@users.noreply.github.com>
2020-06-25 23:14:05 +0100

    Set up enviroment add libraries

--- setup_eval.py
+++ setup_eval.py
@@ -1,7 +1,10 @@
-import requests
+#import requests
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
+#import pandas as pd
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+
+import torchvision
+import torchvision.transforms as transforms
