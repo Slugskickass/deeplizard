41c14d1818386101c8ac9e4ef7a936945acc70dc
CadbyLab <48517894+CadbyLab@users.noreply.github.com>
2020-06-25 17:14:21 +0100

    Set up enviroment

--- setup_eval.py
+++ setup_eval.py
@@ -2,6 +2,6 @@ import requests
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-#import torch
-#import torch.nn as nn
-#import torch.nn.functional as F
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
