732c9b6951454097f96571a21a709e07ff971006
slugskickass <a.cadby@sheffield.ac.uk>
2020-06-26 15:17:59 +0100

    added downloads

--- setup_eval.py
+++ setup_eval.py
@@ -8,3 +8,18 @@ import torch.nn.functional as F
 
 import torchvision
 import torchvision.transforms as transforms
+
+
+train_set = torchvision.datasets.FashionMNIST(
+    root='./data'
+    ,train=True
+    ,download=True
+    ,transform=transforms.Compose([
+        transforms.ToTensor()
+    ])
+)
+
+train_loader = torch.utils.data.DataLoader(train_set
+    ,batch_size=1000
+    ,shuffle=True
+)
